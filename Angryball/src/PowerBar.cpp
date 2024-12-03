b3837360a123188ea295441f77449f9b9eded1f3
Ni Hong Hung <144448680+Wlimni@users.noreply.github.com>
2024-12-03 22:30:28 +0800

    Update PowerBar.cpp

--- Angryball/src/PowerBar.cpp
+++ Angryball/src/PowerBar.cpp
@@ -1,42 +1,38 @@
 #include "PowerBar.h"
 #include <ncurses.h>
 
-// Constructor for initializing the power bar
-PowerBar::PowerBar(int length, int maxLength, int x, int y)
-    : length(length), maxLength(maxLength), x(x), y(y) {}
-
 // Draw the power bar on the screen
-void PowerBar::draw() const {
+void PowerBarRenderer::draw() const {
     // Draw the minimum point of the power bar with '[' at a fixed position
-    mvaddch(y, x, '[');
+    mvaddch(bar.y, bar.x, '[');
 
     // Draw the power bar itself with '=' between the fixed positions
-    for (int i = 1; i <= length; ++i) { // Start drawing after the minimum point
-        mvaddch(y, x + i, '=');
+    for (int i = 1; i <= bar.length; ++i) { // Start drawing after the minimum point
+        mvaddch(bar.y, bar.x + i, '=');
     }
 
     // Draw the maximum point of the power bar with ']' at a fixed position
-    mvaddch(y, x + maxLength, ']');
+    mvaddch(bar.y, bar.x + bar.maxLength, ']');
 }
 
-// Update the length of the power bar by a delta value (positive or negative)
-void PowerBar::update(int delta) {
-    length += delta;
+// Update the power bar's length based on the input (delta)
+void PowerBarRenderer::update(int delta) {
+    bar.length += delta;
 
-    if (length > maxLength) {
-        length = maxLength; // Cap the bar at the maximum length
+    if (bar.length > bar.maxLength) {
+        bar.length = bar.maxLength; // Cap the bar at the maximum length
     }
-    if (length < 1) {
-        length = 0; // Minimum bar length set to 0
+    if (bar.length < 1) {
+        bar.length = 0; // Minimum bar length set to 0
     }
 }
 
-// Check if the power bar is full (at its maximum length)
-bool PowerBar::isFull() const {
-    return length == maxLength; // Return true if the bar is full
+// Check if the power bar is full
+bool PowerBarRenderer::isFull() const {
+    return bar.length == bar.maxLength; // Check if the bar is full
 }
 
-// Reset the power bar to a length of 0
-void PowerBar::reset() {
-    length = 0; // Reset the bar to 0 when used
+// Reset the power bar to 0
+void PowerBarRenderer::reset() {
+    bar.length = 0; // Reset the bar to 0 when used
 }
