e8175266173b7c006cc4048f6ea17cf1d20bc8bd
Ni Hong Hung <144448680+Wlimni@users.noreply.github.com>
2024-12-03 22:17:07 +0800

    Update PowerBar.h

--- Angryball/include/PowerBar.h
+++ Angryball/include/PowerBar.h
@@ -1,14 +1,27 @@
 #ifndef POWERBAR_H
 #define POWERBAR_H
 
-// Power Bar structure with a constructor
-struct PowerBar {
+// PowerBar class declaration
+class PowerBar {
+public:
     int length;    // Length of the power bar
     int maxLength; // Maximum length of the bar
     int x, y;      // Position of the bar
 
-    // Constructor for initializing the power bar
+    // Constructor
     PowerBar(int length, int maxLength, int x, int y);
+
+    // Draw the power bar
+    void draw() const;
+
+    // Update the power bar's length
+    void update(int delta);
+
+    // Check if the power bar is full
+    bool isFull() const;
+
+    // Reset the power bar
+    void reset();
 };
 
-#endif
+#endif // POWERBAR_H
