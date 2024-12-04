0f4e5416e6f7dade5c3780208f432aac9ba650ae
RayanInCode <rayanrahman4017@gmail.com>
2024-12-04 23:12:54 +0800

    Partially working game

--- Angryball/src/BallCollision.cpp
+++ Angryball/src/BallCollision.cpp
@@ -2,11 +2,8 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <ctime>
-
-struct Ball {
-    int x, y;         
-    int dirX, dirY;  
-};
+#include "../include/BallCollision.h"
+#include "../include/BrickGenerator.h"
 
 void initializeBall(Ball &ball) {
     ball.x = 20;          
@@ -15,40 +12,42 @@ void initializeBall(Ball &ball) {
     ball.dirY = 1;        
 }
 
-void moveBall(Ball &ball, int barX, int barLength) {
-   
+void moveBall(Ball &ball, int barX, int barLength, BrickGenerator &brickGen) {
     ball.x += ball.dirX;
     ball.y += ball.dirY;
 
-    
+    // Check for wall collisions
     if (ball.x <= 0 || ball.x >= COLS - 1) {
         ball.dirX *= -1; 
     }
 
-   
     if (ball.y <= 0) {
         ball.dirY *= -1; 
     }
 
-    
+    // Check for collision with the bar
     if (ball.y == LINES - 1 && ball.x >= barX && ball.x < barX + barLength) {
         ball.dirY *= -1; 
-
-        
         int randomAngle = rand() % 3; 
-        if (randomAngle == 0) {
-            ball.dirX = -1; 
-        } else if (randomAngle == 1) {
-            ball.dirX = 0;
-        } else {
-            ball.dirX = 1; 
-        }
-
+        ball.dirX = (randomAngle == 0) ? -1 : (randomAngle == 1) ? 0 : 1;
         ball.y--;
     }
 
+    // Check for collision with bricks
+    for (auto it = brickGen.bricks.begin(); it != brickGen.bricks.end(); ) {
+        if (it->active && ball.y == it->y && ball.x >= it->x && ball.x < it->x + 1) {
+            ball.dirY *= -1; // Bounce off the brick
+            it->active = false; // Remove the brick
+            it = brickGen.bricks.erase(it); // Erase the brick from the vector
+        } else {
+            ++it; // Move to the next brick
+        }
+    }
+
+    // Check if the ball falls below the screen
     if (ball.y >= LINES - 1) {
         ball.dirY = 0; 
+        ball.dirX = 0; 
     }
 }
 
@@ -57,4 +56,3 @@ void drawBar(int barX, int barLength) {
         mvaddch(LINES - 1, barX + i, '#'); 
     }
 }
-
