a4ae74f38255eab564b765ec61f1d20cb8182f1b
Watuthanthrige Omaya Kavindi De Alwis <omaya.dealwis@gmail.com>
2024-12-04 19:48:37 +0800

    Update Menu.h
    header file for menu

---
+++ Angryball/include/Menu.h
@@ -0,0 +1,36 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>  
+#include <ncurses.h> // access to ncurses lib which allows programmers to create textual user interfaces
+
+
+using namespace std;
+
+class Menu {
+private:
+    WINDOW * menuwin;  
+
+    int menu_h = 30; 
+    int menu_w = 150; // height and width of menu are deifned here 
+
+    std::string title[5] = { // ASCII Art of title name to be displayed in menu 
+        " _______                          __           __ __ ", 
+        "|   _   |.-----.-----.----.--.--.|  |--.---.-.|  |  |",
+        "|       ||     |  _  |   _|  |  ||  _  |  _  ||  |  |",
+        "|___|___||__|__|___  |__| |___  ||_____|___._||__|__|",
+        "              |_____|    |_____|                     ",       
+    };
+    
+
+public:
+    Menu();  
+    int display_options();  
+    std::string input_name(int menu_w, int menu_h);
+    void game_info(int y_max, int x_max); 
+    void play_instructions(int y_max, int x_max); 
+};
+
+#endif
+
+     
